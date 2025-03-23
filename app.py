0cb523c00ea5b598b7f7b9732cb2fbf77cf6911b
Akhil <Akilpatthikkal@github.com>
2025-03-23 22:35:28 +0530

    stash message

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template, request, redirect, url_for, session, jsonify, flash  # <-- Add render_template, request, redirect, url_for, session, jsonify, flash
+from flask import Flask, render_template, request, redirect, url_for, session, jsonify, flash  
 import random
 from flask_mail import Mail, Message
 from dotenv import load_dotenv
